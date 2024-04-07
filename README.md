8bfbc79c5af91be6ad2c5d0f7fb84df979d3e44b
iguasty <127635798+iguasty@users.noreply.github.com>
2024-04-06 23:41:16 -0700

    Added full functional menu. Program is functional. Added known bugs to design document. Minor edit to README

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Project_1
 
- Project 1 is creating a class for a complex object. 
- The program runs a menu that the user interacts with attributes of the object from the class. 
+Project 1 is creating a class for a complex object. 
+The program runs a menu that the user interacts with attributes of the object from the class. 
  
- How to use program:
 
  
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+import random
 
 # class declaration
 class Creature:
@@ -15,33 +17,93 @@ class Creature:
         self.leg_count = new_leg_count
         self.ability = new_ability
         self.xp = new_xp
-        self.xp_units ="Level"  # unit attributes defined without variable so they cannot be changed
+        self.xp_units ="XP Level"  # unit attributes defined without variable so they cannot be changed
         self.arm_units = "arms"     
         self.leg_units = "legs"
 
     def upgrade(self):
-        """Upgrade this Creatures XP"""
-        self.xp += 5
+        """Upgrade this Creature's XP"""
+        self.xp += random.randint(1, 50)
+        return self.xp
+    
+    def ability_set(self, ability_num):
+        """Determines ability"""
+        # match case for determining ability type
+        match ability_num:
+            case 1:
+                return "Flight"
+            case 2:
+                return "Invsibility"
+            case 3:
+                return "Fire resistance"
+            case _:
+                return "That is not 1,2 or 3"
+        
 
     def display_attributes(self):
         """Display creatures attributes"""
-        return f"{self.upgrade}:\n"
+        return  f"Name: {self.name}\n" \
+                f"{self.xp_units} {self.xp}\n" \
+                f"Creature type: {self.type}\n" \
+                f"{self.arm_count} {self.arm_units}\n" \
+                f"{self.leg_count} {self.leg_units}\n" \
+                f"Ability: {self.ability_set(self.ability)}"
+               
 
-    def edit_attributes(self):
-        """Edit creatures attributes"""
+ #   def edit_attributes(self, type, name, arm_count, leg_count, ability_num):
+ #       """Edit creatures attributes"""
+ #       return my_creature(type, name, arm_count, leg_count, ability_num)
 
 
-# main 
+# main
 # start program by asking name
+
+os.system('cls') # clears terminal screen 
 print("Welcome to the Creature Creator! What is your name?")
-user_name = input("My name is: ")
+user_name = input("My name is:\n-> ")
+os.system('cls') # clears terminal screen 
 print(f"Hello {user_name}! Time to create your creature.")
 
+# create object instance and ask user input for attribute values
 my_creature = Creature(input("What is the creatures type? (Ex. unicorn)\n-> "),
                        input("What is the creatures name?\n-> "),
-                       int(input("How many arms does the creature have?\n->")),
-                       int(input("How many legs does the creature have? ")),
-                       int(input(f"What ability does your creature have? Choose 1,2, or 3\n 1. Flight\n 2. Invisibility\n 3. Fire resistance\n-> "))
+                       int(input("How many arms does the creature have?\n-> ")),
+                       int(input("How many legs does the creature have?\n-> ")),
+                       int(input("What ability does your creature have? Choose 1,2, or 3\n 1. Flight\n 2. Invisibility\n 3. Fire resistance\n-> "))
                        )
 
-print(my_creature.display_attributes)
+
+os.system('cls') # clears terminal screen 
+
+loop_program = True
+while loop_program:
+    print( "Main Menu:\n\n1. Display Attributes\n2. Edit Attributes\n3. Upgrade XP Level\n4. Exit program")
+    
+    
+    select = int(input("\nEnter selection -> "))
+    
+    if(select != 1 or 2 or 3 or 4):
+        match select:
+            case 1:
+                os.system('cls') # clears terminal screen
+                print(f"Here are your creatures attributes:\n\n{my_creature.display_attributes()}\n") # calls method from class Creature to display objects attributes
+            case 2:
+                os.system('cls') # clears terminal screen
+                my_creature.__init__(input("Set creature type\n-> "),
+                                            input("Set creature name\n-> "),
+                                            int(input("Set arm amount\n-> ")),
+                                            int(input("Set leg amount\n-> ")),
+                                            int(input("Set ability. Choose 1,2, or 3\n 1. Flight\n 2. Invisibility\n 3. Fire resistance\n-> "))
+                                            )
+            case 3:
+                os.system('cls') # clears terminal screen
+                print(f"XP increased to {my_creature.upgrade()}!\n")
+            case 4:
+                os.system('cls') # clears terminal screen
+                print("Goodbye!")
+                loop_program = False
+            case _:
+                os.system('cls') # clears terminal screen
+                print("That is not a valid option. Try again.")
+                
+                
