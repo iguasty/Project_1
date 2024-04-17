162ab961994ac426c390ba73ce742bb6fd28f8f4
iguasty <127635798+iguasty@users.noreply.github.com>
2024-04-17 11:54:27 -0700

    added error handling to all inputs. cleaned up code. changed method class_input to edit_attributes. Generated/updated docstrings for all methods

--- main.py
+++ main.py
@@ -22,27 +22,86 @@ class Creature:
         self.leg_units = "legs"
 
     def upgrade(self):
-        """Upgrade this Creature's XP"""
+        """Upgrades Creature object's XP level with random number 1-50
+
+        Returns:
+            _type_: XP level
+        """
         self.xp += random.randint(1, 50)
         return self.xp
     
     def ability_set(self, ability_num):
-        """Determines ability"""
+        """Assigns ability type to int input
+
+        Args:
+            ability_num (int): 1,2, or 3
+
+        Returns:
+            _type_: ability type
+        """
         # match case for determining ability type
-        match ability_num:
-            case 1:
+        if ability_num == 1:
                 return "Flight"
-            case 2:
+        elif ability_num == 2:
                 return "Invsibility"
-            case 3:
+        elif ability_num == 3:
                 return "Fire resistance"
-            case _:
-                return "Not set"
+    
+    def edit_attributes(self):
+        """Edits the attribute values of class Creature
+
+        Raises:
+            ValueError: raised if self.ability has bad input
+        """
+        while True:
+                try:
+                    self.type = input("Set creature type -> ")
+                    break
+                except:
+                    print("Invalid input. Try again")
+        while True:
+                try:
+                    self.name = input("Set creature name -> ")
+                    break
+                except:
+                    print("Invalid input. Try again")
+        while True:
+                try:
+                    self.arm_count = int(input("Set arm amount -> "))
+                    break
+                except:
+                    print("Invalid input. Try again")
+        while True:
+                try:
+                    self.leg_count = int(input("Set leg amount -> "))
+                    break
+                except:
+                    print("Invalid input. Try again")
+        while True:
+                try:
                     
-        
+                    ability = int(input("What ability does your creature have? Choose 1,2, or 3\n 1. Flight\n 2. Invisibility\n 3. Fire resistance\n-> "))
+                    if ability == 1:
+                        self.ability = ability
+                        break
+                    elif ability == 2:
+                        self.ability = ability
+                        break
+                    elif ability == 3:
+                        self.ability = ability
+                        break
+                    else:
+                        raise ValueError
+                except ValueError:
+                    print("Invalid input. Try again")
+                    
 
     def display_attributes(self):
-        """Display creatures attributes"""
+        """Displays Creature objects attributes
+
+        Returns:
+            _type_: all attributes
+        """
         return  f"Name: {self.name}\n" \
                 f"{self.xp_units} {self.xp}\n" \
                 f"Creature type: {self.type}\n" \
@@ -50,7 +109,7 @@ class Creature:
                 f"{self.leg_count} {self.leg_units}\n" \
                 f"Ability: {self.ability_set(self.ability)}"
                
-# main
+# init
 # start program by asking name
 
 os.system('cls') # clears terminal screen 
@@ -60,46 +119,40 @@ user_name = input("My name is:\n-> ")
 os.system('cls') 
 print(f"Hello {user_name}! Time to create your creature.")
 
-# create object instance and ask user input for attribute values
-my_creature = Creature(input("What is the creatures type? (Ex. unicorn)\n-> "),
-                       input("What is the creatures name?\n-> "),
-                       int(input("How many arms does the creature have?\n-> ")),
-                       int(input("How many legs does the creature have?\n-> ")),
-                       int(input("What ability does your creature have? Choose 1,2, or 3\n 1. Flight\n 2. Invisibility\n 3. Fire resistance\n-> "))
-                       )
-
-
-os.system('cls')
+# creates new Creature object
+my_creature = Creature()
+# calls edit_attributes method to set attributes
+my_creature.edit_attributes()
 
 # enter while loop for menu
-loop_program = True
-while loop_program:
-    print( "Main Menu:\n\n1. Display Attributes\n2. Edit Attributes\n3. Upgrade XP Level\n4. Exit program")
+while True:
     
+    print( "Main Menu:\n\n1. Display Attributes\n2. Edit Attributes\n3. Upgrade XP Level\n4. Exit program")
     select = int(input("\nEnter selection -> ")) # user selects menu item
 
     # match case for selecting menu item
     match select:
         case 1:
+            # display attributes
             os.system('cls') # clears terminal screen
             print(f"Here are your creatures attributes:\n\n{my_creature.display_attributes()}\n") # calls method from class Creature to display objects attributes
         case 2:
+            # edit attributes
             os.system('cls')
-            # sets new values to objects attributes
-            my_creature.__init__(input("Set creature type\n-> "),
-                                        input("Set creature name\n-> "),
-                                        int(input("Set arm amount\n-> ")),
-                                        int(input("Set leg amount\n-> ")),
-                                        int(input("Set ability. Choose 1,2, or 3\n 1. Flight\n 2. Invisibility\n 3. Fire resistance\n-> "))
-                                        )
+            my_creature.edit_attributes()
         case 3:
+            # upgrade XP level
             os.system('cls') 
             print(f"XP increased to {my_creature.upgrade()}!\n")
         case 4:
+            # exit program
             os.system('cls') 
             print("Goodbye!")
-            loop_program = False  # exits program
+            break
         case _:
+            # error handling bad input, returns to main menu
             os.system('cls') 
-            print("That is not a valid option. Try again.") # returns to main menu
+            print("Invalid input. Try again.") # returns to main menu
+            
+            
             
